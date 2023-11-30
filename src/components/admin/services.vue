93820026a4bb5b77e9b1f9089aaa1915f9d1555a
Vanis Manyaga Onchwari <63814849+Manyaga@users.noreply.github.com>
2023-11-30 13:51:26 +0300

    SERVICE

--- src/components/admin/services.vue
+++ src/components/admin/services.vue
@@ -1,51 +1,27 @@
 <template>
-  <div
-    class="modal fade"
-    id="verifyModalContent"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="verifyModalContent"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="verifyModalContent" tabindex="-1" role="dialog" aria-labelledby="verifyModalContent"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="verifyModalContent_title">Add Service</h5>
-          <button
-            class="btn btn-close"
-            type="button"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button class="btn btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <Form @submit="addService" :validation-schema="schema" class="user">
           <div class="modal-body">
             <div class="form-group">
-              <label class="col-form-label" for="service_name"
-                >Service Name:</label
-              >
-              <Field
-                name="service_name"
-                class="form-control"
-                id="service_name"
-                type="text"
-              />
+              <label class="col-form-label" for="service_name">Service Name:</label>
+              <Field name="service_name" class="form-control" id="service_name" type="text" />
               <ErrorMessage name="service_name" class="text-danger p-3" />
             </div>
             <div class="form-group">
-              <label class="col-form-label" for="description"
-                >Description:</label
-              >
+              <label class="col-form-label" for="description">Description:</label>
               <Field name="description" class="form-control" type="text" />
               <ErrorMessage name="description" class="text-danger p-3" />
             </div>
           </div>
           <div class="modal-footer">
-            <button
-              class="btn btn-secondary"
-              type="button"
-              data-bs-dismiss="modal"
-            >
+            <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">
               Close
             </button>
             <button class="btn btn-primary" type="Submit">Submit</button>
@@ -55,60 +31,31 @@
     </div>
   </div>
   <!-- Edit Country Modal -->
-  <div
-    class="modal fade"
-    id="editModalContent"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="editModalContent"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="editModalContent" tabindex="-1" role="dialog" aria-labelledby="editModalContent"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="verifyModalContent_title">
             Edit Service
           </h5>
-          <button
-            class="btn btn-close"
-            type="button"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+          <button class="btn btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <Form @submit="editService" :validation-schema="schema" class="user">
           <div class="modal-body">
             <div class="form-group">
-              <label class="col-form-label" for="service_name"
-                >Service Name:</label
-              >
-              <Field
-                name="service_name"
-                class="form-control"
-                v-model="service_name"
-                type="text"
-              />
+              <label class="col-form-label" for="service_name">Service Name:</label>
+              <Field name="service_name" class="form-control" v-model="service_name" type="text" />
               <ErrorMessage name="service_name" class="text-danger p-3" />
             </div>
             <div class="form-group">
-              <label class="col-form-label" for="description"
-                >Description:</label
-              >
-              <Field
-                name="description"
-                class="form-control"
-                v-model="description"
-                type="text"
-              />
+              <label class="col-form-label" for="description">Description:</label>
+              <Field name="description" class="form-control" v-model="description" type="text" />
               <ErrorMessage name="description" class="text-danger p-3" />
             </div>
           </div>
           <div class="modal-footer">
-            <button
-              class="btn btn-secondary"
-              type="button"
-              data-bs-dismiss="modal"
-            >
+            <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">
               Close
             </button>
             <button class="btn btn-primary" type="Submit">Submit</button>
@@ -122,59 +69,34 @@
     <Topbar />
     <div class="main-content-wrap d-flex flex-column">
       <div class="main-content">
-        <Breadcrumbs />
-        <div class="separator-breadcrumb border-top"></div>
-        <div class="row mb-2">
-          <div class="col-md-12">
-            <button
-              class="btn btn-light text-white ul-btn-raised--v2 m-1 float-end"
-              type="button"
-              data-bs-toggle="modal"
-              data-target="#verifyModalContent"
-              data-whatever="@mdo"
-            >
+            <button class="btn btn-primary text-white ul-btn-raised--v2 m-1 float-end" type="button"
+              data-bs-toggle="modal" data-target="#verifyModalContent" data-whatever="@mdo">
               <i class="nav-icon i-add text-primary text-white fw-bold"></i> ADD
               SERVICE
             </button>
-          </div>
-        </div>
-        <div class="row mb-4">
-          <div class="col-md-12">
-            <div class="table-responsive">
-              <table class="table text-center" id="service_table">
-                <thead>
-                  <tr class="bg-primary text-white">
-                    <th scope="col">#</th>
-                    <th scope="col">Service Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr
-                    v-for="(service, index) in services"
-                    :key="service.service_id"
-                  >
-                    <th scope="row">{{ index + 1 }}</th>
-                    <td>{{ service.service_name }}</td>
-                    <td>{{ service.description }}</td>
-                    <td>
-                      <a
-                        class="text-success me-2"
-                        @click="openEditService(service)"
-                        ><i class="nav-icon i-Pen-2 fw-bold"></i
-                      ></a>
-                      <a
-                        class="text-danger me-2 float-end"
-                        @click="deleteService(service.service_id)"
-                        ><i class="nav-icon i-Close-Window fw-bold"></i
-                      ></a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
+        <Breadcrumbs />
+        <div class="separator-breadcrumb border-top"></div>
+
+        <div class="row mb-4" v-for="(service, index) in services" :key="service.service_id">
+          <div class="col-md-6">
+            <div class="card card-ecommerce-3 o-hidden mb-4">
+              <div class="d-flex flex-column flex-sm-row">
+                <div><img class="card-img-left" src="../../assets/images/fashion.png" alt="" /></div>
+                <div class="flex-grow-1 p-4">
+                  <h5 class="m-0">{{ service.service_name }}</h5>
+                  <p class="text-muted mt-3">
+                    {{ service.description }} </p>
+                  <div class="actions">
+                    <button class="btn btn-primary ul-btn-raised--v2 m-1" @click="openEditService(service)"
+                      type="button">EDIT</button>
+                    <button class="btn btn-danger ul-btn-raised--v2 m-1 float-end"
+                      @click="deleteService(service.service_id)" type="button">DELETE</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+
         </div>
       </div>
       <div class="flex-grow-1"></div>
@@ -188,24 +110,7 @@ import Topbar from "@/components/partials/Topbar.vue";
 import Footer from "@/components/partials/Footer.vue";
 import Sidebar from "@/components/partials/Sidebar";
 import Breadcrumbs from "@/components/partials/Breadcrumbs";
-import "datatables.net-dt/js/dataTables.dataTables";
-import "@/assets/css/dataTables.bootstrap4.min.css";
-import "@/assets/css/buttons.dataTables.min.css";
-import "datatables.net-buttons/js/dataTables.buttons.js";
-import "datatables.net-buttons/js/buttons.colVis.js";
-import "datatables.net-buttons/js/buttons.flash.js";
-import "datatables.net-buttons/js/buttons.html5.js";
-import "datatables.net-buttons/js/buttons.print.js";
-import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-import "@/assets/datatables/jquery.dataTables.min.js";
-import "@/assets/datatables/dataTables.bootstrap4.min.js";
-import "@/assets/datatables/dataTables.buttons.min.js";
-import "@/assets/datatables/buttons.html5.min.js";
-import "@/assets/datatables/buttons.print.min.js";
-import "@/assets/datatables/jszip.min.js";
 import {
   ALL_SERVICES_QUERY,
   ADD_SERVICE_MUTATION,
@@ -360,7 +265,6 @@ export default {
     },
     async statusChange() {
       this.services = [];
-      $("#service_table").DataTable().destroy();
       await this.$apollo
         .query({
           query: ALL_SERVICES_QUERY,
@@ -368,34 +272,6 @@ export default {
         .then((response) => {
           this.services = response.data.services;
         });
-      setTimeout(function () {
-        $("#service_table").DataTable({
-          destroy: true,
-          pageLength: 5,
-          lengthChange: true,
-          processing: true,
-          paging: true,
-          info: false,
-          dom: "Bfrtip",
-          buttons: [
-            {
-              extend: "csv",
-              text: '<i class="fa-solid fa-file-pdf"></i>',
-              className: "btn btn-sm btn-outline-success mb-3 text-success",
-            },
-            {
-              extend: "pdf",
-              text: '<i class="fa fa-file-pdf"></i>',
-              className: "btn btn-sm btn-outline-danger mb-3 text-danger",
-            },
-            {
-              extend: "print",
-              text: '<i class="fa fa-print"></i>',
-              className: "btn btn-sm btn-outline-secondary mb-3 text-secondary",
-            },
-          ],
-        });
-      }, 300);
     },
   },
   async created() {
