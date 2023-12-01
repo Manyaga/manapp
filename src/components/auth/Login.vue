ac4ab6ce1a3f17d489ee58aa688efbda2bce0a85
Vanis Manyaga Onchwari <63814849+Manyaga@users.noreply.github.com>
2023-12-01 04:32:56 +0300

    Update Login.vue

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -6,10 +6,7 @@
           <div class="signin-image">
             <br />
             <figure>
-              <img
-                src="../../assets/images/wrist-watch.jpg"
-                alt="sing up image"
-              />
+              <img src="../../assets/images/wrist-watch.jpg" alt="sing up image" />
             </figure>
             <a href="/register" class="signup-image-link">Create an account</a>
           </div>
@@ -23,55 +20,25 @@
               </div>
             </div>
             <h3 class="form-title">Welcome Back!</h3>
-            <Form
-              @submit="handleLogin"
-              :validation-schema="schema"
-              class="user"
-            >
+            <Form @submit="handleLogin" :validation-schema="schema" class="user">
               <div class="form-group">
-                <Field
-                  name="email"
-                  type="text"
-                  class="form-control form-control-user"
-                  placeholder="your-email@gmail.com"
-                />
+                <Field name="email" type="text" class="form-control form-control-user"
+                  placeholder="your-email@gmail.com" />
                 <ErrorMessage name="email" class="text-danger p-3" />
               </div>
               <div class="form-group">
                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                <Field
-                  name="password"
-                  type="password"
-                  class="form-control form-control-user"
-                  placeholder="Password"
-                />
+                <Field name="password" type="password" class="form-control form-control-user" placeholder="Password" />
                 <ErrorMessage name="password" class="text-danger p-3" />
               </div>
               <div class="form-group">
-                <input
-                  type="checkbox"
-                  name="remember-me"
-                  id="remember-me"
-                  class="agree-term"
-                />
-                <label for="remember-me" class="label-agree-term"
-                  ><span><span></span></span>Remember me</label
-                >
+                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
+                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
               </div>
               <div class="form-group form-button">
-                <input
-                  type="submit"
-                  name="signin"
-                  id="signin"
-                  class="form-submit"
-                  value="Log in"
-                />
+                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in" />
               </div>
-              <div
-                v-if="message"
-                class="alert alert-dismissible fade show alert-card alert-danger"
-                role="alert"
-              >
+              <div v-if="message" class="alert alert-dismissible fade show alert-card alert-danger" role="alert">
                 <span style="font-weight: bold; font-size: 16px">{{
                   message
                 }}</span>
@@ -124,34 +91,32 @@ export default {
   },
   methods: {
     async handleLogin(user) {
-      // await this.$apollo
-      //   .mutate({
-      //     mutation: LOGIN_MUTATION,
-      //     variables: {
-      //       username: user.email,
-      //       password: user.password,
-      //     },
-      //   })
-      //   .then((response) => {
-      //     console.log(response.data);
-      //     localStorage.setItem("token", response.data.login.token);
-      //   })
-      //   .catch((error) => {
-      //     this.message = error.message;
-      //   });
-      // await this.$apollo
-      //   .query({
-      //     query: CURRENTUSER_QUERY,
-      //     fetchPolicy: "network-only",
-      //   })
-      //   .then((response) => {
-      //     TokenService.setUser(response.data.currentUser);
-      //   });
+      await this.$apollo
+        .mutate({
+          mutation: LOGIN_MUTATION,
+          variables: {
+            username: user.email,
+            password: user.password,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          localStorage.setItem("token", response.data.login.token);
+        })
+        .catch((error) => {
+          this.message = error.message;
+        });
+      await this.$apollo
+        .query({
+          query: CURRENTUSER_QUERY,
+          fetchPolicy: "network-only",
+        })
+        .then((response) => {
+          TokenService.setUser(response.data.currentUser);
+        });
 
       // // this.message = "We apologize, but we are currently unable to process your login. Kindly attempt to log in again later.";
-      // this.$router.push("/dashboard");
-      this.message =
-        "We apologize, but we are currently unable to process your login. Kindly attempt to log in again later.";
+      this.$router.push("/dashboard");
     },
   },
 };
